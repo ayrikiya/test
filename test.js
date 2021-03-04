8d273d019d76ac2393f538986a2b5cc618e4511d
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2021-03-04 16:26:02 +0800

    change test.js

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-var log = function() {
-    console.log.apply(console, arguments)
-}
+class Button {
+    constructor(value) {
+      this.value = value;
+    }
+    click = function() {
+      alert(this.value);
+    }
+  }
+  
+  let button = new Button("hello");
+  
+  setTimeout(button.click, 1000); // hello
+
+
+  function foo() {
+      
+  }
